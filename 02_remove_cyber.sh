ce3a553c6759832836cd5024c2ec4c0788116481
NiosZ <58999172+niosz@users.noreply.github.com>
2024-09-01 01:05:37 +0200

    Update 02_remove_cyber.sh

--- 02_remove_cyber.sh
+++ 02_remove_cyber.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
-CHECK="$(csrutil status | awk -F: '{print $2}' | tr -d ' .')";
-if [ "$CHECK" = "disabled" ]; then
-	echo "Before this command you must disactivate System Integrity Protection.";
-	echo "  1) reboot your mac in recovery mode";
-	echo "  2) csrutil enable";
-	exit 1;
+# ------------------------------------------------------------------------------------- script configuration
+MUST_SIP_DISABLED=1;
+MUST_IS_USER_ROOT=1;
+MUST_HAVE_NODE_JS=0;
+
+# ------------------------------------------------------------------------------------- protection disabled
+if [ $MUST_SIP_DISABLED -eq 1 ]; then
+	CHECK="$(csrutil status | awk -F: '{print $2}' | tr -d ' .')";
+	if [ "$CHECK" = "enabled" ]; then
+		echo "Before this command you must disactivate System Integrity Protection.";
+		echo "  1) reboot your mac in recovery mode";
+		echo "  2) csrutil disable";
+		exit 1;
+	fi;
 fi;
-sudo systemextensionsctl list | grep cyberark | awk '{print "sudo systemextensionsctl uninstall "$3" "$4}' | bash
+
+# ------------------------------------------------------------------------------------- run user is root
+if [ $MUST_IS_USER_ROOT -eq 1 ]; then
+	CHECK="$(id | tr '()' '§§' | awk -F'§' '{print $2}')";
+	if [ ! "$CHECK" = "root" ]; then
+		echo "This command must be run from root user.";
+		exit 1;
+	fi; 
+fi;
+
+# ------------------------------------------------------------------------------------- nodejs is installed
+if [ $MUST_HAVE_NODE_JS -eq 1 ]; then
+	CHECK="$(node -e 'console.log(2+2);' 2>/dev/null)";
+	if [ $CHECK -ne 4 ]; then
+		echo "NodeJS is not present on machine, must be installed for run.";
+		exit 1;
+	fi;
+fi;
+
+# ------------------------------------------------------------------------------------- main code
+
+systemextensionsctl list | grep cyberark | awk '{print "systemextensionsctl uninstall "$3" "$4}' | bash
