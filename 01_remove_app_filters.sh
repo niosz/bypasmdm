5df0e11e5cc3bccf8103ccf46e918243704e7a04
NiosZ <58999172+niosz@users.noreply.github.com>
2024-09-01 01:06:18 +0200

    Update 01_remove_app_filters.sh

--- 01_remove_app_filters.sh
+++ 01_remove_app_filters.sh
@@ -1,2 +1,38 @@
 #!/bin/bash
-sudo node -e 'const fs=require("fs");var fcg="/Library/Application Support/JAMF/.jmf_settings.json";var cfg=JSON.parse(fs.readFileSync(fcg,"utf8"));cfg.blacklist=[];fs.writeFileSync(fcg,JSON.stringify(cfg,null,2),"utf8");';
+# ------------------------------------------------------------------------------------- script configuration
+MUST_SIP_DISABLED=0;
+MUST_IS_USER_ROOT=1;
+MUST_HAVE_NODE_JS=1;
+
+# ------------------------------------------------------------------------------------- protection disabled
+if [ $MUST_SIP_DISABLED -eq 1 ]; then
+	CHECK="$(csrutil status | awk -F: '{print $2}' | tr -d ' .')";
+	if [ "$CHECK" = "enabled" ]; then
+		echo "Before this command you must disactivate System Integrity Protection.";
+		echo "  1) reboot your mac in recovery mode";
+		echo "  2) csrutil disable";
+		exit 1;
+	fi;
+fi;
+
+# ------------------------------------------------------------------------------------- run user is root
+if [ $MUST_IS_USER_ROOT -eq 1 ]; then
+	CHECK="$(id | tr '()' '§§' | awk -F'§' '{print $2}')";
+	if [ ! "$CHECK" = "root" ]; then
+		echo "This command must be run from root user.";
+		exit 1;
+	fi; 
+fi;
+
+# ------------------------------------------------------------------------------------- nodejs is installed
+if [ $MUST_HAVE_NODE_JS -eq 1 ]; then
+	CHECK="$(node -e 'console.log(2+2);' 2>/dev/null)";
+	if [ $CHECK -ne 4 ]; then
+		echo "NodeJS is not present on machine, must be installed for run.";
+		exit 1;
+	fi;
+fi;
+
+# ------------------------------------------------------------------------------------- main code
+
+node -e 'const fs=require("fs");var fcg="/Library/Application Support/JAMF/.jmf_settings.json";var cfg=JSON.parse(fs.readFileSync(fcg,"utf8"));cfg.blacklist=[];fs.writeFileSync(fcg,JSON.stringify(cfg,null,2),"utf8");';
